241b01e404caede376dd6de202ec3f7b5e08dc8e
ishaandesai <IshaanDesai21@gmail.com>
2025-11-25 23:12:00 -0600

    Add FTC Dash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ASlingTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ASlingTele.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.draw;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
@@ -15,9 +18,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Supplier;
 
-import org.firstinspires.ftc.teamcode.NextFTC.misc.LocalizationMath;
 import org.firstinspires.ftc.teamcode.misc.gamepad.GamepadMapping;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.pedroPathing.Drawing;
 import org.firstinspires.ftc.teamcode.subsystems.robot.Robot;
 import org.firstinspires.ftc.teamcode.teleop.fsm.FSM;
 
@@ -53,9 +56,6 @@ public class ASlingTele extends OpMode {
                 .addPath(new Path(new BezierLine(follower::getPose, new Pose(45, 98))))
                 .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.8))
                 .build();
-
-        localizationMath = new LocalizationMath();
-
     }
 
 
@@ -63,7 +63,7 @@ public class ASlingTele extends OpMode {
     public void start() {
         // run once when we start
         robot.hardwareSoftReset();
-        follower.startTeleOpDrive(true);
+        follower.startTeleopDrive(true);
     }
 
 
@@ -74,6 +74,8 @@ public class ASlingTele extends OpMode {
         //Potential error with both?
         telemetryM.update();
         telemetryM.update(telemetry);
+        Pose pose = follower.getPose();
+
 
 
 //----------------------------Telemetry and Dash Field Overlay----------------------------\\
@@ -101,11 +103,8 @@ public class ASlingTele extends OpMode {
         Drawing.drawRobot(packet.fieldOverlay(), pose);
         FtcDashboard.getInstance().sendTelemetryPacket(packet);
 
-        Pose p = follower.getPose();
-
 //----------------------------Localization Math----------------------------\\
 
-        Pose pose = follower.getPose();
 
         double dist = LocalizationMath.getRedDistance(pose);
         double err  = LocalizationMath.getRedHeadingError(pose);
