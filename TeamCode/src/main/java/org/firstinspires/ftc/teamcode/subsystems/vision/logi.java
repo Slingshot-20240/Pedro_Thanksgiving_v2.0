c0c88f682e7ae468e8b022a3728279521c0a9b0c
Jining Liu <hello@jining.dev>
2025-12-04 19:23:17 -0600

    cam stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/logi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/logi.java
@@ -24,7 +24,7 @@ public class logi {
 
     final double ATsize = 6.5;
     final double theta = Math.toRadians(45.2189284116);
-    final double[] resolution = new double[]{640, 480};
+    final double[] resolution = new double[]{1920, 1080};
 
     public logi(HardwareMap hw) {
         apriltagPipeline = new AprilTagProcessor.Builder()
