5a4069d55844f1c24c7490c76a499a1ab976f53b
Aubrey Tipps <aatipps@gmail.com>
2026-01-04 21:38:10 -0600

    updating nextftc turret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextFTC/subsystems_nf/Turretnf.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextFTC/subsystems_nf/Turretnf.java
@@ -5,7 +5,12 @@ import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.hardware.controllable.RunToPosition;
+import dev.nextftc.hardware.controllable.RunToState;
 import dev.nextftc.hardware.impl.CRServoEx;
+import dev.nextftc.hardware.impl.FeedbackCRServoEx;
+import dev.nextftc.hardware.impl.FeedbackServoEx;
 import dev.nextftc.hardware.impl.ServoEx;
 import dev.nextftc.hardware.positionable.SetPosition;
 import dev.nextftc.hardware.powerable.SetPower;
@@ -16,13 +21,31 @@ public class Turretnf implements Subsystem {
     private Turretnf() {
     }
 
-    public ServoEx turretServo1;
-    public ServoEx turretServo2;
+    public FeedbackServoEx turretServo1 = new FeedbackServoEx("analog0", "turret1", 0.01);
+    public FeedbackServoEx turretServo2 = new FeedbackServoEx("analog1", "turret2", 0.01);
+
+    // TODO got from docs idk we'll see :sk
+
+    double totalAngle = 0.0; // This is your angle of the servo
+    double previousAngle = 0.0; // This is the previous loop's servo position
+
+    void updatePosition() {
+        double currentAngle = turretServo1.getCurrentPosition();
+        double deltaAngle = currentAngle - previousAngle;
+
+        if (deltaAngle > Math.PI) deltaAngle -= 2 * Math.PI;
+        else if (deltaAngle < -Math.PI) deltaAngle += 2 * Math.PI;
+
+        totalAngle += deltaAngle;
+        previousAngle = currentAngle;
+    }
+
+    // TODO lowkey dont know what method to use bc it's an angle so
 
     public Command farBlue() {
         return new ParallelGroup(
                 new SetPosition(turretServo1, 0),
-                new SetPosition(turretServo2, 0)
+                new SetPosition(turretServo1, 0)
         );
     }
 
