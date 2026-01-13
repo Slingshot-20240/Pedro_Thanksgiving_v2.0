66a5024e6222863e4f7474f72025f7d2389b68b0
ishaandesai <IshaanDesai21@gmail.com>
2026-01-13 00:57:15 -0600

    shooter me kmskmskms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextFTC/config/FTCDashShooterPIDNextFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextFTC/config/FTCDashShooterPIDNextFTC.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.NextFTC.config;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
 import dev.nextftc.control.feedback.PIDCoefficients;
 import dev.nextftc.control.feedforward.BasicFeedforwardParameters;
 import dev.nextftc.hardware.controllable.MotorGroup;
@@ -14,13 +19,15 @@ import dev.nextftc.hardware.impl.MotorEx;
 @TeleOp(name = "FTC Dash Shooter Config")
 public class FTCDashShooterPIDNextFTC extends OpMode {
 
+    private Telemetry dashboardTelemetry;
+
     MotorEx outtake1, outtake2;
     MotorGroup shooter;
 
     public static double p = 0.0005, i = 0.0, d = 0.0;
     public static double kV = 0, kA = 0, kS = 0;
 
-    public static int shooterVel = 1100;
+    public static int targetShooterVel = 1100;
 
     public static PIDCoefficients coefficients = new PIDCoefficients(p, i, d);
     public static BasicFeedforwardParameters feedforwardParameters = new BasicFeedforwardParameters(kV, kA, kS);
@@ -37,6 +44,9 @@ public class FTCDashShooterPIDNextFTC extends OpMode {
         outtake2.reverse();
 
         shooter = new MotorGroup(outtake1, outtake2);
+
+        dashboardTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
     }
 
     @Override
@@ -46,13 +56,14 @@ public class FTCDashShooterPIDNextFTC extends OpMode {
 //                        new KineticState(0.0, shooter.getVelocity())
 //                )
 //        );
+
+        //TODO - figure out how to actually use the target shooter vel into this controller
         shooter.setPower(shooterController.calculate(shooter.getState()));
 
 
-
-        telemetry.addData("Target Velocity", shooterVel);
-        telemetry.addData("Current Velocity", shooter.getVelocity());
-        telemetry.update();
+        dashboardTelemetry.addData("Target Velocity", targetShooterVel);
+        dashboardTelemetry.addData("Current Velocity", shooter.getVelocity());
+        dashboardTelemetry.update();
     }
 }
 
