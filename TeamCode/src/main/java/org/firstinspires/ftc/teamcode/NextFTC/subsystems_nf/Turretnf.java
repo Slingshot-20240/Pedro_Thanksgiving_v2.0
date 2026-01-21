ef191d668234abd304a215d436cf11372eaa40c9
ishaandesai <IshaanDesai21@gmail.com>
2026-01-20 21:49:55 -0600

    New Turret Code NOT DONE!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextFTC/subsystems_nf/Turretnf.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NextFTC/subsystems_nf/Turretnf.java
@@ -1,99 +1,115 @@
 package org.firstinspires.ftc.teamcode.NextFTC.subsystems_nf;
 
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.math.Vector;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
 import dev.nextftc.control.ControlSystem;
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.control.KineticState;
+import dev.nextftc.control.feedback.PIDCoefficients;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.ActiveOpMode;
-import dev.nextftc.hardware.controllable.RunToPosition;
-;
-import dev.nextftc.hardware.impl.FeedbackCRServoEx;
-
-
+import dev.nextftc.hardware.impl.CRServoEx;
 public class Turretnf implements Subsystem {
+
+    public static double distance;
+    public static PIDCoefficients turretPIDCoefficients =
+            new PIDCoefficients(0.005, 0.0, 0);
+
     public static final Turretnf INSTANCE = new Turretnf();
-    private Turretnf() { }
+    private Turretnf() {}
 
-    public FeedbackCRServoEx turretServo1;
-    public FeedbackCRServoEx turretServo2;
+    private CRServoEx turretServoL = new CRServoEx("tL", 0.03);
+    private CRServoEx turretServoR = new CRServoEx("tR", 0.03);
 
-    private final ControlSystem turretController = ControlSystem.builder()
-            .posPid(0.345, 0, 0.001)
-            .basicFF(0.002)
+    public AnalogInput encoderL;
+    public AnalogInput encoderR;
+
+
+    public ControlSystem turretPIDController = ControlSystem.builder()
+            .posPid(turretPIDCoefficients)
             .build();
 
-    // TODO got from docs idk we'll see :sk
 
-    double totalAngle = 0.0; // This is your angle of the servo
-    double previousAngle = 0.0; // This is the previous loop's servo position
+    public static Double targetTurretAng = 0.0;
+    private static double offset = 4.0;
+    private static final double GEAR_RATIO = 3.25;
+    private static final double TURRET_MIN_DEG = -85;
+    private static final double TURRET_MAX_DEG =  85;
+    public static boolean AUTO_AIM = true;
 
-    void updatePosition() {
-        double currentAngle = turretServo1.getCurrentPosition();
-        double deltaAngle = currentAngle - previousAngle;
-
-        if (deltaAngle > Math.PI) deltaAngle -= 2 * Math.PI;
-        else if (deltaAngle < -Math.PI) deltaAngle += 2 * Math.PI;
-
-        totalAngle += deltaAngle;
-        previousAngle = currentAngle;
-    }
-
-    // TODO lowkey dont know what method to use bc it's an angle so
-
-
-    public Command setTurretAngle(double angleDeg) {
-        return new InstantCommand( () ->
-                new RunToPosition(turretController, Math.toRadians(angleDeg))
-        ).addRequirements(turretServo1, turretServo2);
-    }
-    public Command farBlue() {
-        return new InstantCommand( () ->
-                new RunToPosition(turretController, Math.toRadians(180))
-        ).addRequirements(turretServo1, turretServo2);
-    }
-
-
-//    public Command closeBlue() {
-//        return new ParallelGroup(
-//                new RunToPosition(turretServo1, 0),
-//                new RunToPosition(turretServo2, 0)
-//        );
-//    }
-//
-//    public Command farRed() {
-//        return new ParallelGroup(
-//                new RunToPosition(turretServo1, 0),
-//                new RunToPosition(turretServo2, 0)
-//        );
-//    }
-//
-//    public Command closeRed() {
-//        return new ParallelGroup(
-//                new RunToPosition(turretServo1, 0),
-//                new RunToPosition(turretServo2, 0)
-//        );
-//    }
-
+    Pose goal = new Pose(144,144);
 
 
     @Override
     public void initialize() {
-        turretServo1 = new FeedbackCRServoEx(
-                0.01,
-                () ->  ActiveOpMode.hardwareMap().analogInput.get("analog0") ,
-                () ->  ActiveOpMode.hardwareMap().crservo.get("t1")
-        );
-        turretServo2 = new FeedbackCRServoEx(
-                0.01,
-                () ->  ActiveOpMode.hardwareMap().analogInput.get("analog1") ,
-                () ->  ActiveOpMode.hardwareMap().crservo.get("t2")
-        );
+        encoderL = ActiveOpMode.hardwareMap().get(AnalogInput.class, "tLe");
+        encoderR = ActiveOpMode.hardwareMap().get(AnalogInput.class, "tRe");
     }
 
     @Override
     public void periodic() {
-        turretServo1.setPower(turretController.calculate(turretServo1.getState()));
-        turretServo2.setPower(turretController.calculate(turretServo2.getState()));
+
+        if (AUTO_AIM) {
+            turretLoop();
+        }
+        distanceFunc();
+        turretPIDController.setGoal(new KineticState(targetTurretAng));
+        double power = turretPIDController.calculate(new KineticState(getTurretDegrees()));
+
+        turretServoL.setPower(power);
+        turretServoR.setPower(power);
 
     }
+
+    public void turretLoop(){
+
+        double dx = goal.getX() - PedroComponent.follower().getPose().getX();
+        double dy = goal.getY() - PedroComponent.follower().getPose().getY();
+
+        double goalFieldDeg = Math.toDegrees(Math.atan2(dy, dx));
+        double headingDeg   = Math.toDegrees(PedroComponent.follower().getHeading());
+
+        // turret relative to robot forward 0 degree facing to goal side
+        double desired = normalizeAngle(goalFieldDeg - headingDeg);
+        targetTurretAng = clamp(desired, TURRET_MIN_DEG, TURRET_MAX_DEG);
+
+    }
+    public void velocityBasedTurret(){
+        Vector robot2Goal = new Vector(
+                144-PedroComponent.follower().getPose().getX(),
+                144-PedroComponent.follower().getPose().getY()
+        );
+        Vector velocity = PedroComponent.follower().getVelocity();
+        double heading = PedroComponent.follower().getHeading();
+        double coordTheta = velocity.getTheta() - robot2Goal.getTheta();
+        double perpComponent = Math.sin(coordTheta) * velocity.getMagnitude();
+
+        distance = Math.hypot(144-PedroComponent.follower().getPose().getX(), 144-PedroComponent.follower().getPose().getY());
+    }
+
+
+    private double distanceFunc(){
+        return distance = Math.hypot(goal.getX()-PedroComponent.follower().getPose().getX(), goal.getY()-PedroComponent.follower().getPose().getY());
+    }
+
+    private static double clamp(double v, double lo, double hi) {
+        return Math.max(lo, Math.min(hi, v));
+    }
+    public double normalizeAngle(double angDeg) {
+        double ang = angDeg;
+        if (ang < 0.0) ang += 360.0;
+        if (ang > 180.0) ang -= 360.0;
+        return ang;
+    }
+
+
+    private double getTurretDegrees() {
+        double servoDeg = (encoderR.getVoltage() / 3.3) * 360.0;
+
+        // convert to turret degrees
+        return normalizeAngle(servoDeg / GEAR_RATIO);
+    }
+
 }
